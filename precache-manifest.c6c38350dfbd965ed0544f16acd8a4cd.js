80f1c45b124422aed0d095539021a6a77f46ddd4
Aziz Firmansyah Ramawandani <aframawandani@gmail.com>
2024-04-21 07:42:44 +0000

    Initial commit

---
+++ precache-manifest.c6c38350dfbd965ed0544f16acd8a4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab279c185c1be20154acd04907ec8469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3bbc697f1cf6c011ad",
+    "url": "/static/css/main.426b5c8b.chunk.css"
+  },
+  {
+    "revision": "1f5929bab9e74fc2d234",
+    "url": "/static/js/2.a71b1702.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "6a3bbc697f1cf6c011ad",
+    "url": "/static/js/main.eccce0a4.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  }
+]);
